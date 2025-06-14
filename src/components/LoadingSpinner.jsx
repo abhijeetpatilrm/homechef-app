9b23bb08bd6a278de6f6def7fd6e217cad0857bf
ABHIJEET PATIL <153955913+abhijeetpatilrm@users.noreply.github.com>
2025-06-14 23:09:16 +0530

    Update LoadingSpinner.jsx

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -1,10 +1,1 @@
-import React from "react";
-import { CircularProgress, Box } from "@mui/material";
 
-export default function LoadingSpinner() {
-  return (
-    <Box display="flex" justifyContent="center" my={4}>
-      <CircularProgress />
-    </Box>
-  );
-}
