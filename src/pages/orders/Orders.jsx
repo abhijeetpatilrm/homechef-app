1a8f23d2ad771f79eb2f27b54c1b376461697553
ABHIJEET PATIL <153955913+abhijeetpatilrm@users.noreply.github.com>
2025-06-14 23:11:55 +0530

    Update Orders.jsx

--- src/pages/orders/Orders.jsx
+++ src/pages/orders/Orders.jsx
@@ -1,11 +1,1 @@
-import React from "react";
 
-export default function Orders() {
-  return (
-    <div style={{ padding: "2rem" }}>
-      <h1>Your Orders</h1>
-      <p>This is where you will see the list of your orders.</p>
-      {/* TODO: Add order list UI here */}
-    </div>
-  );
-}
